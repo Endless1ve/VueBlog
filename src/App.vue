5631bcd7a29a3c17fb81b73324045c0d47c1a963
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-03-12 19:58:48 +0300

    add variables and fonts

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/normalize.scss";
+@import "@/assets/styles/fonts.scss";
+</style>
